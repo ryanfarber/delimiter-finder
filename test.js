ea0f90e4b354a7405230823967a354b57e026dce
Ryan Farber <32393930+ryanfarber@users.noreply.github.com>
2021-01-06 01:24:51 -0800

    update

--- test.js
+++ test.js
@@ -6,12 +6,3 @@ let delim = new DelimiterFinder(["$", "%"])
 
 let matches = delim.find("hello $world$, how are you %today%")
 console.log(matches)
-
-/*
-
-[
-  { delimiter: '$', matches: [ 'world' ] },
-  { delimiter: '%', matches: [ 'today' ] }
-]
-
-*/
